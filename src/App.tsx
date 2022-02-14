a0bacfa7ae9497aa3efb1baa79bc475ee77068b7
Rohan Yarlagadda <rohanyarlagadda@wifi-roaming-128-4-66-113.wifi.udel.edu>
2022-02-14 16:24:37 -0500

    Added button, header color, image to site

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,31 @@ function App(): JSX.Element {
         <div className="App">
             <h1>First Website!</h1>
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Name: Rohan Yarlagdda
+                UD CISC275 with React Hooks and TypeScript Name: Rohan
+                Yarlagadda
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <div>
+                <button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </button>
+            </div>
             <Container>
                 <Row>
-                    <Col>First Column</Col>
+                    <Col>
+                        First Column
+                        <div>
+                            <img
+                                src="https://64.media.tumblr.com/dc7c8f66d80dd18c19b09b8fb92aac04/682eec13776ef06f-39/s540x810/2a92b320b89255721958619c066edf2de74a1da6.jpg"
+                                alt="Remy from Ratatouille"
+                                width="300"
+                                height="300"
+                            />
+                        </div>
+                    </Col>
                     <Col>Second Column</Col>
                 </Row>
             </Container>

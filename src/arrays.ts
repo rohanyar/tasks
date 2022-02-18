d9e1c800181e6294d880eb76f3d2092fcc05981e
rohanyar <rohan3yarlagadda@gmail.com>
2022-02-18 12:53:56 -0500

    finished first function of arrays

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,13 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    const numLen = numbers.length;
+    if (numLen === 0) {
+        return [];
+    } else {
+        const x = [numbers[0], numbers[numbers.length - 1]];
+        return x;
+    }
 }
 
 /**

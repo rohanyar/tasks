d8de3dde923c6c82e30f03fb0b664c954ea17dcc
rohanyar <rohan3yarlagadda@gmail.com>
2022-03-15 01:19:11 -0400

    finished two dice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,41 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [dice1, setDice1] = useState<number>(1);
+    const [dice2, setDice2] = useState<number>(3);
+    const rollLeft = (
+        <div>
+            <Button
+                onClick={() => {
+                    setDice1(d6);
+                }}
+            >
+                Roll Left
+            </Button>
+        </div>
+    );
+    const rollRight = (
+        <div>
+            <Button
+                onClick={() => {
+                    setDice2(d6);
+                }}
+            >
+                Roll Right
+            </Button>
+        </div>
+    );
+    return (
+        <div>
+            <span data-testid="left-die">{dice1}</span>
+            <span data-testid="right-die">{dice2}</span>
+            <div>
+                {rollLeft} {rollRight}
+            </div>
+            <div>
+                {dice1 === dice2 && dice1 !== 1 && <p>Win</p>}
+                {dice1 === dice2 && dice2 === 1 && <p>Lose</p>}
+            </div>
+        </div>
+    );
 }

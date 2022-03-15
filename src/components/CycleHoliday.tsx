e03698fa2c193015ebb4f07b87e3d7ff957484d3
rohanyar <rohan3yarlagadda@gmail.com>
2022-03-15 14:39:56 -0400

    finished cycle holiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,43 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+// Holidays
+// Christmas: 🎄
+// New Years: 🍾
+// Halloween: 🎃
+// Thanksgiving: 🦃
+// Valentines Day: ❤️
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🍾");
+    const byYear: Record<string, string> = {
+        "🍾": "❤️",
+        "❤️": "🎃",
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🍾"
+    };
+    const byLetter: Record<string, string> = {
+        "🎄": "🎃",
+        "🎃": "🍾",
+        "🍾": "🦃",
+        "🦃": "❤️",
+        "❤️": "🎄"
+    };
+
+    return (
+        <div>
+            <p>Holiday: {holiday}</p>
+            <div>
+                <Button onClick={() => setHoliday(byYear[holiday])}>
+                    Advance by Year
+                </Button>
+            </div>
+            <div>
+                <Button onClick={() => setHoliday(byLetter[holiday])}>
+                    Advance by Alphabet
+                </Button>
+            </div>
+        </div>
+    );
 }
